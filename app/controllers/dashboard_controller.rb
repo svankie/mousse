018a35031608800ea057baac613f295e4ec79771
Karl Roßmann <airongstring@gmail.com>
2013-08-15 20:39:41 -0300

    About page.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -4,4 +4,7 @@ class DashboardController < ApplicationController
   def index
     @thangs_by_date = Thang.all.group_by(&:date)
   end
+
+  def about
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Mousse::Application.routes.draw do
   resources :users
   resources :thangs
 
+  get 'about' => 'dashboard#about'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
