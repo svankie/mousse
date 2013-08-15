0d2c2bb3c3d0e2dadad02567264c187f520bf356
Karl Roßmann <airongstring@gmail.com>
2013-08-15 01:43:47 -0300

    Typo on ANOTHER named URL.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,6 +16,6 @@ class SessionsController < ApplicationController
 
   def destroy
     session[:user_id] = nil
-    redirect_to root, notice: "Logged outta here, bitch."
+    redirect_to root_url, notice: "Logged outta here, bitch."
   end
 end
