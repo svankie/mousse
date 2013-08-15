5d299e2011df3def299f2213e7f1558e153af825
Karl Roßmann <airongstring@gmail.com>
2013-08-15 17:32:22 -0300

    Forgot.

--- app/models/thang.rb
+++ app/models/thang.rb
@@ -1,5 +1,5 @@
 class Thang < ActiveRecord::Base
-  validates :what, :where, :when, :description, presence: true
+  validates :what, :place, :date, :description, presence: true
   validates :what, uniqueness: true
   validates :amount, numericality: { greater_than_or_equal_to: 0.0 }
 end
