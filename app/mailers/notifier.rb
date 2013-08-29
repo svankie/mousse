cc8c16c517d369b27f9445f94d98b6b14eda5c15
Karl Roßmann <airongstring@gmail.com>
2013-08-29 18:17:06 -0300

    How did it occur to me to put view-specific code in controller code? Truncating text is view-related. And truncate is a view helper method.

--- app/mailers/notifier.rb
+++ app/mailers/notifier.rb
@@ -2,9 +2,8 @@ class Notifier < ActionMailer::Base
   default from: "airOnGString@gmail.com"
 
 	def send_pageviews_report
-		title_length = 15
 	  @thangs = Thang.active.inject(Hash.new) do |res, thang|
-			res[truncate(thang.what, length: title_length)] = thang.pageviews
+			res[thang.what] = thang.pageviews
 			res
 		end
 		mail(to: "svankie@gmail.com", subject: "Weekly pageview report from Recco").deliver
